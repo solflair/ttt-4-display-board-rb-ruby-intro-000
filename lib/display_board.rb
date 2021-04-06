98bc930ea32c770c7e829a5200aa9f08a25fab98
Jasmine Alexander <solflair.jewelry@gmail.com>
2021-04-06 00:17:42 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,5 @@
 # Define display_board that accepts a board and prints
+<<<<<<< HEAD
 # out the current state.
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -6,4 +7,13 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+board = [" "," "," "," "," "," "," "," "," "]# out the current state.
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts "----------------------------------------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts "----------------------------------------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+>>>>>>> b7350243513a8ff9856e8bcfe9c3b6ff548dd30a
 end
